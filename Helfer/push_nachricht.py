20f55aeb21d4f33b4b175bb0fa80f9fd29f7893c
Emanuel <91322964+emmadula22@users.noreply.github.com>
2022-12-01 13:56:54 +0100

    Kommentar von Emanuel hinzugefügt

--- Helfer/push_nachricht.py
+++ Helfer/push_nachricht.py
@@ -1,16 +1,19 @@
 from pushnotifier import PushNotifier as pn
 
-#pn = pn.PushNotifier('username', 'password', 'package_name', 'api_key')
-pn = pn.PushNotifier('dhbwprojektsmarthome', 'Smarthome22', 'pushNotifier', '8E7TV75BBV46B5HB6TE7T52VBETFBFBBTTFBETFBFB')
 
-pn.login('Smarthome22')
-{
-    'username': 'dhbwprojektsmarthome',
-    'avatar': 'https://gravatar.com/avatar/faedc831a6c911569d1830cfd2a89af9?s=128&d=wavatar&r=x',
-    'app_token': '8E7TV75BBV46B5HB6TE7T52VBETFBFBBTTFBETFBFB',
-}
+class push_nachricht:
 
-pn.send_text('text', devices=['ID1', 'ID2'], silent=True)
+    #pn = pn.PushNotifier('username', 'password', 'package_name', 'api_key')
+    pn = pn.PushNotifier('dhbwprojektsmarthome', 'Smarthome22', 'pushNotifier', '8E7TV75BBV46B5HB6TE7T52VBETFBFBBTTFBETFBFB')
+
+    pn.login('Smarthome22')
+    {
+        'username': 'dhbwprojektsmarthome',
+        'avatar': 'https://gravatar.com/avatar/faedc831a6c911569d1830cfd2a89af9?s=128&d=wavatar&r=x',
+        'app_token': '8E7TV75BBV46B5HB6TE7T52VBETFBFBBTTFBETFBFB',
+    }
+
+    pn.send_text('text', devices=['ID1', 'ID2'], silent=True)
 
 
 

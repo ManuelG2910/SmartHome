a3390a626956a194e416e7e6e20755824f7d0913
Emanuel <91322964+emmadula22@users.noreply.github.com>
2022-12-01 14:07:53 +0100

    Kommentar von Emanuel hinzugefügt

--- Hausautomations/warmwasserboiler.py
+++ Hausautomations/warmwasserboiler.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 aktivierungszeit= None
 statusaktiv = "aus"
 startzeit = datetime.now()
-endzeit = datetime.now().
+endzeit = datetime.now()
 def aktivieren():
     statusaktiv= "an"
     print(statusaktiv)
